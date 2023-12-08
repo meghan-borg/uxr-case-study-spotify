376ffdcf4b49c95130bee8472c734a0fb6efa1ca
Meghan Borg <meghanborg1@gmail.com>
2023-12-08 11:34:28 -0500

    Clean up files

--- 5_scripts/2023-11-03_analyses.R
+++ 5_scripts/2023-11-03_analyses.R
@@ -23,6 +23,7 @@ data_sub <- data %>%
            album == "1989" | album == "1989 (Taylor's Version)")
 
 # Group by album and calculate the average popularity
+
 data_sub <- data_sub %>%
   group_by(album) %>%
   summarize(average_popularity = mean(popularity))
@@ -33,6 +34,7 @@ data_sub <- data_sub %>%
     TRUE ~ 1  # Default value for other cases
   ))
 
+# Create album_rec variable
 
 data_sub <- data_sub %>% 
   mutate(album_rec = case_when(album == "Red" | album == "Red (Taylor's Version)" ~ "Red",
@@ -40,19 +42,23 @@ data_sub <- data_sub %>%
                                album == "Fearless" | album == "Fearless (Taylor's Version)" ~ "Fearless",
                                album == "1989" | album == "1989 (Taylor's Version)" ~ "1989"))
 
-# Reorder TV factor
+# Reorder album_rec for plots
 
 new_order <- c("Fearless", "Speak Now", "Red", "1989")
 data_sub$album_rec <- factor(data_sub$album_rec, levels = new_order)
+
+# Reorder album for plots
 
 new_order2 <- c("1989", "1989 (Taylor's Version)", "Red", "Red (Taylor's Version)", 
                 "Speak Now", "Speak Now (Taylor's Version)", "Fearless", "Fearless (Taylor's Version)")
 data_sub$album <- factor(data_sub$album, levels = new_order2)
 
-
-
+# Set tv_status as a factor
 
 data_sub$tv_status <- factor(data_sub$tv_status, labels = c("Original Version", "Taylor's Version"))
+
+
+# Bar plot 1
 
 ggplot(data_sub, aes(fill=tv_status, y=average_popularity, x=album_rec)) + 
   geom_bar(position="dodge", stat="identity") + 
@@ -65,6 +71,8 @@ ggplot(data_sub, aes(fill=tv_status, y=average_popularity, x=album_rec)) +
         plot.title = element_text(hjust= 0.5, face = "bold")) +
   labs(title = "\n Spotify Popularity of Original Albums vs Taylor's Version", y = "\n Popularity", x = "\n Album", fill = "Album Version") +
   scale_fill_manual(values = c("#B0B0B0", "#5C5C5C"))
+
+# Bar plot 2
 
 ggplot(data_sub, aes(fill=album, y=average_popularity, x=album)) +
   geom_bar(position="dodge", stat="identity") +
@@ -89,6 +97,8 @@ ggplot(data_sub, aes(fill=album, y=average_popularity, x=album)) +
   scale_fill_manual(values = c("#93CDDF","#3281B5","#BB777A", "#97373D","#ADAAC8","#6A62A1","#EDD685","#E2A641")) +
   theme(axis.text.y = element_text(margin = margin(r = -30)))
 
+# Combine plots
+
 # library(ggpubr)
 # fullplot <- ggarrange(plot1, plot2, nrow = 1, ncol = 2)
 # annotate_figure(fullplot, top = text_grob("Spotify Popularity of Original Albums vs Taylor's Version", color = "black", face = "bold", size = "25"))
@@ -97,96 +107,4 @@ ggplot(data_sub, aes(fill=album, y=average_popularity, x=album)) +
 
 
 
-data$tv_status <- factor(data$tv_status)
-str(data$tv_status)
-
-gghistogram(data_sub, x = "popularity", y = "count", fill = "tv_status")
-
-
-# Stacked barplot with multiple groups
-ggplot(data=data_sub, aes(x=album, y=average_popularity, fill=album)) +
-  geom_bar(stat="identity")
-# Use position=position_dodge()
-ggplot(data=data_sub, aes(x=dose, y=len, fill=supp)) +
-  geom_bar(stat="identity", position=position_dodge())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-## ---- Interactive 
-ggplotly(plot) 
-
-## ---- Grouped Scatter plot with density plots
-library(ggpubr)
-plot <- ggscatterhist(data, x = "release_date", y = "popularity",  
-              color = "tv_status", size = 3, alpha = 0.6,  palette = c("#E7B800", "#00AFBB"),  margin.params = list(fill = "tv_status", color = "black", size = 0.4))
-
-library(ggthemes)
-library(plotly)
-(scatter_plot<-data %>% 
-    ggplot(aes( x = release_date, y = popularity, color = as_factor(tv_status))) +     # describe how data should be mapped to features of the plot
-    geom_point(position = "jitter", alpha=0.8) +                                               #transparency of the dots; jitter adds noise to data so the dots aren't stacked on top of each other.
-    labs(x="Social Anxiety Year 1", y= "Social Anxiety Year 4", # labels
-         title="The Association between Social Anxiety at Year 1 and 4 by Sex") +               # title
-    geom_smooth(method=lm, se=TRUE)+                                                           #add regression line without se
-    #facet_wrap(~as_factor(aa_A3_Sex))+                                                        #create separate plots for sex
-    geom_rangeframe(color= "black") +                                                           #add outline around plots in black
-    theme_tufte(base_size = 16) +                                                              # add a theme https://ggplot2.tidyverse.org/reference/ggtheme.html
-    labs(color="Gender")+ #legend title
-    scale_color_manual(values = c("#993F00", "steelblue")))                                    #https://r-charts.com/color-palettes/
-
-
-
-
-
-
-library(ggplot2)
-library(plotly)
-
-library(ggplot2)
-library(plotly)
-
-# Check unique values in the tv_status column
-unique(data$tv_status)
-
-# If there are unexpected values, remap them to valid colors or factors
-data$tv_status <- ifelse(data$tv_status %in% c("ValidValue1", "ValidValue2"), data$tv_status, "Other")
-
-# Create the ggplot2 plot
-p <- ggplot(data, aes(x = release_date, y = popularity, text = name)) +
-  geom_point(aes(color = tv_status, size = 3, alpha = 0.6), show.legend = TRUE, palette = c("#E7B800", "#00AFBB")) +
-  scale_alpha(range = c(0.2, 0.8)) +
-  labs(x = "Release Date", y = "Popularity") +
-  theme_minimal() +
-  theme(legend.position = "top") +
-  geom_point(aes(fill = tv_status, color = "black"), size = 0.4) +
-  scale_fill_identity() +
-  scale_color_identity() +
-  theme(legend.title = element_blank())
-
-# Convert ggplot2 plot to Plotly
-p <- ggplotly(p, tooltip = "text")
-
-# Display the interactive Plotly plot
-p
 
